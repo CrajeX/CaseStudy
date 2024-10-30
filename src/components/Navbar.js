f1155c4fdb3a0fe252227c63cb606d93a6b53bac
CrajeX <crajeextremeyt@gmail.com>
2024-10-30 20:00:22 +0800

    Fix styles.css path issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from 'C:\\Users\\USER\\job-board-app\\src\\styles.css';
-import './styles.css';
+// import './styles.css';
 const Navbar = ({ userType, onLogout }) => {
     return (
         <nav id="navbar">
